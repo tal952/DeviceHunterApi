b5f1d281c99c7452db1f2d53fe3213ce801dba6e
Tal Cherniak <talc@soluto.com>
2016-09-07 14:43:40 +0300

    add distanceFromBeacon

--- server.js
+++ server.js
@@ -5,25 +5,27 @@ const body = require('koa-parse-json');
 const app = koa();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB||'mongodb://localhost:27017');
+mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017');
 
 app.use(body());
 
-var Metrics = mongoose.model('Metrics', new mongoose.Schema({
-    _id: String,
-    Model:String,
-    NearestBeaconId:String,
-}, { _id: false }));
+const distanceFromBeacon = new mongoose.Schema({_id: String, distance: Number}, {_id: false});
 
-app.use(_.get('/api/isAlive', function *(){
+const Metrics = mongoose.model('Metrics', new mongoose.Schema({
+    _id: String,
+    model: String,
+    distanceFromBeacon: [distanceFromBeacon]
+}, {_id: false}));
+
+app.use(_.get('/api/isAlive', function *() {
 }));
-app.use(_.get('/api/metrics', function *(){
+app.use(_.get('/api/metrics', function *() {
     this.body = yield Metrics.find();
 }));
-app.use(_.post('/api/metrics/:id', function *(id){
+app.use(_.post('/api/metrics/:id', function *(id) {
     const newMetric = new Metrics(this.request.body);
     newMetric._id = id;
-    yield Metrics.update({ _id: id },newMetric,{upsert: true});
+    yield Metrics.update({_id: id}, newMetric, {upsert: true});
 }));
 
 app.listen(process.env.PORT || 5000);
