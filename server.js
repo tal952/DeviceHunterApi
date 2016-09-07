dc4497ca4ac72d8f87ca4a75cb4a97db48cc220d
Tal Cherniak <talc@soluto.com>
2016-09-07 14:29:29 +0300

    working :)

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+const mongoose = require('mongoose');
 const koa = require('koa');
 const _ = require('koa-route');
+const body = require('koa-parse-json');
 const app = koa();
 
-var db = {
-    tobi: { name: 'tobi', species: 'ferret' },
-    loki: { name: 'loki', species: 'ferret' },
-    jane: { name: 'jane', species: 'ferret' }
-};
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.PROD_MONGODB||'mongodb://localhost:27017');
 
-var pets = {
-    list: function *(){
-        var names = Object.keys(db);
-        this.body = 'pets: ' + names.join(', ');
-    }
-};
+app.use(body());
 
-app.use(_.get('/pets', pets.list));
+var Metrics = mongoose.model('Metrics', new mongoose.Schema({
+    _id: String,
+    Model:String,
+    NearestBeaconId:String,
+}, { _id: false }));
+
+app.use(_.get('/api/isAlive', function *(){
+}));
+app.use(_.get('/api/metrics', function *(){
+    this.body = yield Metrics.find();
+}));
+app.use(_.post('/api/metrics/:id', function *(id){
+    const newMetric = new Metrics(this.request.body);
+    newMetric._id = id;
+    yield Metrics.update({ _id: id },newMetric,{upsert: true});
+}));
 
 app.listen(process.env.PORT || 5000);
 console.log('listening on port 5000');
